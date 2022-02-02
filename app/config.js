c5d978fbeabc2c2feac41881150c5a3d0cf80114
k-tb <88774801+k-tb@users.noreply.github.com>
2022-02-02 16:07:12 +0000

    update index

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Check how good your service is for users',
 
   // Default port that prototype runs on
   port: '3000',
